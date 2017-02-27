c69f229f6a7242bae836195bd61302bcca2619a7
Mika Kolari <mika.kolari@soikea.com>
2017-02-27 23:08:42 +0200

    Model refactoring

--- Crash.Fit.Core/Nutrition/INutritionRepository.cs
+++ Crash.Fit.Core/Nutrition/INutritionRepository.cs
@@ -9,18 +9,21 @@ namespace Crash.Fit.Nutrition
     public interface INutritionRepository
     {
         IEnumerable<Nutrient> GetNutrients();
+        bool CreateNutrient(Nutrient nutrient);
+        bool UpdateNutrient(Nutrient nutrient);
         IEnumerable<DailyIntake> GetDailyIntakes(Gender gender, TimeSpan age);
 
 
-        IEnumerable<FoodMinimal> SearchFoods(string[] nameTokens, Guid? userId = null );
+        IEnumerable<FoodMinimal> SearchFoods(string[] nameTokens, Guid? userId = null);
         FoodDetails GetFood(Guid id);
         IEnumerable<FoodDetails> GetFoods(IEnumerable<Guid> ids);
+        //IEnumerable<FoodDetails> GetFoods(IEnumerable<Guid> ids);
         bool CreateFood(FoodDetails food);
         bool UpdateFood(FoodDetails food);
         bool DeleteFood(FoodMinimal food);
         bool RestoreFood(Guid id, out FoodDetails food);
 
-        IEnumerable<Portion> GetPortions(IEnumerable<Guid> ids);
+        //IEnumerable<Portion> GetPortions(IEnumerable<Guid> ids);
 
         IEnumerable<MealDetails> SearchMeals(Guid userId, DateTimeOffset start, DateTimeOffset end);
         MealDetails GetMeal(Guid id);
--- Crash.Fit.Core/Nutrition/NutritionRepository.cs
+++ Crash.Fit.Core/Nutrition/NutritionRepository.cs
@@ -4,10 +4,11 @@ using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace Crash.Fit.Nutrition
 {
-    public class NutritionRepository : RepositoryBase
+    public class NutritionRepository : RepositoryBase, INutritionRepository
     {
         public NutritionRepository(DbProviderFactory dbFactory, string connectionString) : base(dbFactory,connectionString)
         {
@@ -15,48 +16,369 @@ namespace Crash.Fit.Nutrition
 
         public IEnumerable<Nutrient> GetNutrients()
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT * FROM Nutrient";
+            using (var conn = CreateConnection())
+            {
+                return conn.Query<Nutrient>(sql);
+            }
+        }
+        public bool CreateNutrient(Nutrient nutrient)
+        {
+            nutrient.Id = Guid.NewGuid().ToString();
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("INSERT INTO Nutrient(Id,Name,ShortName,Unit,FineliId,FineliClass,FineliGroup,UIOrder,UIVisible) VALUES(@Id,@Name,@ShortName,@Unit,@FineliId,@FineliClass,@FineliGroup,@UIOrder,@UIVisible)", nutrient, tran);
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    nutrient.Id = null;
+                    return false;
+                }
+            }
+        }
+        public bool UpdateNutrient(Nutrient nutrient)
+        {
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("UPDATE Nutrient SET Name=@Name, ShortName=@ShortName, Unit=@Unit, FineliId=@FineliId, FineliClass=@FineliClass, FineliGroup=@FineliGroup, UIOrder=@UIOrder, UIVisible=@UIVisible WHERE Id=@Id", nutrient, tran);
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public IEnumerable<DailyIntake> GetDailyIntakes(Gender gender, TimeSpan age)
+        {
+            var sql = @"
+SELECT * FROM DailyIntake 
+WHERE Gender = @gender AND StartAge <= @age AND EndAge >= @age";
+            var parameters = new { gender = gender.ToString(), age = age.TotalDays/365 };
+            using (var conn = CreateConnection())
+            {
+                return conn.Query<DailyIntake>(sql, parameters);
+            }
         }
         public FoodDetails GetFood(Guid id)
         {
-            throw new NotImplementedException();
+            return GetFoods(new[] { id }).FirstOrDefault();
         }
-        public IEnumerable<FoodDetails> SearchFoods(Guid userId, params string[] nameParts)
+        public IEnumerable<FoodDetails> GetFoods(IEnumerable<Guid> ids)
         {
-            throw new NotImplementedException();
+            var sql = @"
+SELECT * FROM Food WHERE Id IN(@ids);
+SELECT * FROM FoodPortion WHERE FoodId IN(@ids);
+SELECT * FROM FoodNutrient WHERE FoodId IN(@ids);
+SELECT * FROM RecipeIngredient WHERE RecipeId IN(@ids) ORDER BY [Index]";
+            var parameters = new { ids };
+            using (var conn = CreateConnection())
+            using (var multi = conn.QueryMultiple(sql, parameters))
+            {
+                var foods = multi.Read<FoodDetails>().ToArray();
+                var portions = multi.Read<PortionRaw>().ToArray();
+                var nutrients = multi.Read<FoodNutrientAmount>().ToArray();
+                var ingredients = multi.Read<RecipeIngredientRaw>().ToArray();
+                foreach(var food in foods)
+                {
+                    food.Portions = portions.Where(p => p.FoodId == food.Id).ToArray();
+                    food.Nutrients = nutrients.Where(p => p.FoodId == food.Id).ToArray();
+                    food.Ingredients = ingredients.Where(p => p.FoodId == food.Id).ToArray();
+                }
+                return foods;
+            }
         }
-        public void CreateFood(FoodDetails food)
+        public IEnumerable<FoodMinimal> SearchFoods(string[] nameTokens, Guid? userId = null)
         {
-
+            var parameters = new DynamicParameters();
+            var sql = "";
+            for (int i = 0; i < nameTokens.Length; i++)
+            {
+                parameters.Add("p" + i, nameTokens[i]);
+                sql += " AND Name LIKE CONCAT('%',@p" + i + ",'%')";
+            }
+            if (userId.HasValue)
+            {
+                sql += " AND (UserId IS NULL OR UserId=@UserId)";
+                parameters.Add("UserId", userId.Value);
+            }
+            else
+            {
+                sql += " AND UserId IS NULL";
+            }
+            sql = "SELECT * FROM Food WHERE " + sql.Substring(5) + " ORDER BY Name";
+            using (var conn = CreateConnection())
+            {
+                return conn.Query<FoodMinimal>(sql, parameters);
+            }
         }
-        public void UpdateFood(FoodDetails food)
+        public bool CreateFood(FoodDetails food)
         {
-
+            food.Id = Guid.NewGuid();
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("INSERT INTO Food(Id,UserId,Name,IsRecipe,FineliId) VALUES(@Id,@UserId,@Name,@IsRecipe,@FineliId)", food, tran);
+                    conn.Execute("INSERT INTO FoodNutrient(FoodId,NutrientId,Amount) VALUES(@FoodId,@NutrientId,@Amount)",
+                        food.Nutrients.Select(n => new { FoodId = food.Id, n.NutrientId, n.Amount }), tran);
+                    conn.Execute("INSERT INTO FoodPortion(Id,FoodId,Name,Weight) VALUES(@Id,@FoodId,@Name,@Weight)", food.Portions.Select(p => new { Id = Guid.NewGuid(), FoodId = food.Id, p.Name, p.Weight }), tran);
+                    if(food.Ingredients != null)
+                    {
+                        conn.Execute("INSERT INTO RecipeIngredient(RecipeId,[Index],IngredientId,PortionId,PortionAmount,Weight) VALUES(@FoodId,@Index,@IngredientId,@PortionId,@PortionAmount,@Weight)", food.Ingredients.Select(i => new
+                        {
+                            FoodId = food.Id,
+                            i.Index,
+                            i.IngredientId,
+                            i.PortionId,
+                            i.PortionAmount,
+                            i.Weight
+                        }), tran);
+                    }
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    food.Id = Guid.Empty;
+                    tran.Rollback();
+                    return false;
+                }
+            }
         }
-        public void DeleteFood(FoodDetails food)
+        public bool UpdateFood(FoodDetails food)
         {
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("DELETE FROM FoodNutrient WHERE FoodId=@Id", new { Id = food.Id }, tran);
+                    conn.Execute("DELETE FROM FoodPortion WHERE FoodId=@Id", new { Id = food.Id }, tran);
+                    conn.Execute("DELETE FROM RecipeIngredient WHERE FoodId=@Id", new { Id = food.Id }, tran);
 
+                    conn.Execute("UPDATE Food SET Name=@Name WHERE Id=@Id", food, tran);
+                    conn.Execute("INSERT INTO FoodNutrient(FoodId,NutrientId,Amount) VALUES(@FoodId,@NutrientId,@Amount)",
+                        food.Nutrients.Select(n => new { FoodId = food.Id, n.NutrientId, n.Amount }), tran);
+                    conn.Execute("INSERT INTO FoodPortion(Id,FoodId,Name,Weight) VALUES(@FoodId,@Name,@Weight)", food.Portions.Select(p => new {p.Id, FoodId = food.Id, p.Name, p.Weight }), tran);
+                    if (food.Ingredients != null)
+                    {
+                        conn.Execute("INSERT INTO RecipeIngredient(RecipeId,[Index],IngredientId,PortionId,PortionAmount,Weight) VALUES(@FoodId,@Index,@IngredientId,@PortionId,@PortionAmount,@Weight)", food.Ingredients.Select(i => new
+                        {
+                            FoodId = food.Id,
+                            i.Index,
+                            i.IngredientId,
+                            i.PortionId,
+                            i.PortionAmount,
+                            i.Weight
+                        }), tran);
+                    }
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    tran.Rollback();
+                    return false;
+                }
+            }
         }
-
+        public bool DeleteFood(FoodMinimal food)
+        {
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("UPDATE Food SET Deleted=@Deleted WHERE Id=@Id", new { Id = food.Id, Deleted = DateTimeOffset.Now }, tran);
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    tran.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool RestoreFood(Guid id, out FoodDetails food)
+        {
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("UPDATE Food SET Deleted=NULL WHERE Id=@Id", new { Id = id }, tran);
+                    tran.Commit();
+                    food = GetFood(id);
+                    return true;
+                }
+                catch
+                {
+                    tran.Rollback();
+                    food = null;
+                    return false;
+                }
+            }
+        }
         public MealDetails GetMeal(Guid id)
         {
-            throw new NotImplementedException();
+            var sql = @"
+SELECT * FROM Meal WHERE Id=@id;
+SELECT * FROM MealNutrient WHERE MealId=@id;
+SELECT * FROM MealRow WHERE MealId=@id ORDER BY [Index];";
+            var parameters = new { id };
+            using (var conn = CreateConnection())
+            using (var multi = conn.QueryMultiple(sql, parameters))
+            {
+                var meal = multi.Read<MealDetails>().SingleOrDefault();
+                if (meal != null)
+                {
+                    meal.Nutrients = multi.Read<NutrientAmount>().ToArray();
+                    meal.Rows = multi.Read<MealRow>().ToArray();
+                }
+                return meal;
+            }
         }
-        public IEnumerable<MealDetails> GetMeals(Guid userId, DateTimeOffset start, DateTimeOffset end)
+        public IEnumerable<MealDetails> SearchMeals(Guid userId, DateTimeOffset start, DateTimeOffset end)
         {
-            throw new NotImplementedException();
+            var sql = @"
+SELECT * FROM Meal WHERE UserId=@userId AND Time >= @start AND Time <= @end AND Deleted IS NULL;
+SELECT * FROM MealNutrient WHERE MealId IN (SELECT Id FROM Meal WHERE UserId=@userId AND Time >= @start AND Time <= @end AND Deleted IS NULL);";
+            var parameters = new { userId, start, end };
+            using (var conn = CreateConnection())
+            using (var multi = conn.QueryMultiple(sql, parameters))
+            {
+                var meals = multi.Read<MealDetails>().ToList();
+                var nutrients = multi.Read<MealNutrient>().ToList();
+                foreach (var meal in meals)
+                {
+                    meal.Nutrients = nutrients.Where(n => n.MealId == meal.Id).Cast<NutrientAmount>().ToArray();
+                }
+                return meals;
+            }
         }
-        public void CreateMeal(MealDetails Meal)
+        public bool CreateMeal(MealDetails meal)
         {
+            meal.Id = Guid.NewGuid();
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("INSERT INTO Meal(Id,UserId,Time,Name) VALUES(@Id,@UserId,@Time,@Name)", meal, tran);
+                    conn.Execute("INSERT INTO MealNutrient(MealId,NutrientId,Amount) VALUES(@MealId,@NutrientId,@Amount)",
+                        meal.Nutrients.Select(n => new { MealId = meal.Id, n.NutrientId, n.Amount }), tran);
+                    conn.Execute("INSERT INTO MealRow(MealId,[Index],FoodId,Quantity,PortionId,Weight) VALUES(@MealId,@Index,@FoodId,@Quantity,@PortionId,@Weight)", meal.Rows.Select(r => new
+                    {
+                        MealId = meal.Id,
+                        r.Index,
+                        r.FoodId,
+                        r.Quantity,
+                        r.PortionId,
+                        r.Weight }), tran);
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    meal.Id = Guid.Empty;
+                    tran.Rollback();
+                    return false;
+                }
+            }
+        }
+        public bool UpdateMeal(MealDetails meal)
+        {
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("DELETE FROM MealNutrient WHERE MealId=@Id", new { Id = meal.Id }, tran);
+                    conn.Execute("DELETE FROM MealRow WHERE MealId=@Id", new { Id = meal.Id }, tran);
 
+                    conn.Execute("UPDATE Meal SET Time=@Time, Name=@Name WHERE Id=@Id", meal, tran);
+                    conn.Execute("INSERT INTO MealNutrient(MealId,NutrientId,Amount) VALUES(@MealId,@NutrientId,@Amount)",
+                                            meal.Nutrients.Select(n => new { MealId = meal.Id, n.NutrientId, n.Amount }), tran);
+                    conn.Execute("INSERT INTO MealRow(MealId,[Index],FoodId,Quantity,PortionId,Weight) VALUES(@MealId,@Index,@FoodId,@Quantity,@PortionId,@Weight)", meal.Rows.Select(r => new
+                    {
+                        MealId = meal.Id,
+                        r.Index,
+                        r.FoodId,
+                        r.Quantity,
+                        r.PortionId,
+                        r.Weight }), tran);
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    tran.Rollback();
+                    return false;
+                }
+            }
         }
-        public void UpdateMeal(MealDetails Meal)
+        public bool DeleteMeal(MealMinimal meal)
         {
-
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("UPDATE Meal SET Deleted=@Deleted WHERE Id=@Id", new { Id = meal.Id, Deleted = DateTimeOffset.Now }, tran);
+                    tran.Commit();
+                    return true;
+                }
+                catch
+                {
+                    tran.Rollback();
+                    return false;
+                }
+            }
         }
-        public void DeleteMeal(MealDetails Meal)
+        public bool RestoreMeal(Guid id, out MealDetails meal)
         {
+            using (var conn = CreateConnection())
+            using (var tran = conn.BeginTransaction())
+            {
+                try
+                {
+                    conn.Execute("UPDATE Meal SET Deleted=NULL WHERE Id=@Id", new { Id = id }, tran);
+                    tran.Commit();
+                    meal = GetMeal(id);
+                    return true;
+                }
+                catch
+                {
+                    tran.Rollback();
+                    meal = null;
+                    return false;
+                }
+            }
+        }
 
+        private class PortionRaw : Portion
+        {
+            public Guid FoodId { get; set; }
+        }
+        private class FoodNutrientAmount : NutrientAmount
+        {
+            public Guid FoodId { get; set; }
+        }
+        private class RecipeIngredientRaw : RecipeIngredient
+        {
+            public Guid FoodId { get; set; }
         }
     }
 }
