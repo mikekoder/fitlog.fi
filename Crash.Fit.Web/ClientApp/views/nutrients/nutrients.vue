19c472d345b8fdeac5e98dc35806df2e342f2974
mikekoder <mkoderfi@gmail.com>
2017-08-15 22:34:07 +0300

    Nutrients layout more mobile friendly

--- Crash.Fit.Web/ClientApp/views/nutrients/nutrients.vue
+++ Crash.Fit.Web/ClientApp/views/nutrients/nutrients.vue
@@ -4,39 +4,59 @@
         <section class="content">
             <div class="row">
                 <div class="col-sm-12">
-                    <table class="nutrient-settings">
-                        <thead>
-                            <tr><th></th><th></th><th></th><th colspan="2">{{ $t("visibility") }}</th></tr>
-                            <tr><th></th><th></th><th></th><th>{{ $t("summary") }}</th><th>{{ $t("details") }}</th></tr>
-                        </thead>
-                        <tbody v-for="group in groups">
-                            <tr><th></th><th colspan="2">{{ $t(group.id) }}</th></tr>
-                            <tr v-for="(nutrient, index) in nutrientSettings[group.id]">
-                                <td>
+                    <div class="row hidden-xs">
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-3"></div>
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-6 center"><strong>{{ $t("visibility") }}</strong></div>
+                    </div>
+                    <div class="row hidden-xs">
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-3"></div>
+                        <div class="col-sm-1"></div>
+                        <div class="col-sm-3"><strong>{{ $t("summary") }}</strong></div>
+                        <div class="col-sm-3"><strong>{{ $t("details") }}</strong></div>
+                    </div>
+                    <template v-for="group in groups">
+                        <div class="row"><div class="col-sm-12"><h4>{{ $t(group.id) }}</h4></div></div>
+                        <template v-for="(nutrient, index) in nutrientSettings[group.id]">
+                            <div class="row">
+                                <div class="col-xs-4 col-sm-2">
                                     <button class="btn btn-sm" @click="moveNutrientUp(group.id, index)" :disabled="index === 0"><i class="fa fa-arrow-up"></i></button>
                                     <button class="btn btn-sm" @click="moveNutrientDown(group.id, index)" :disabled="index === (nutrientSettings[group.id].length - 1)"><i class="fa fa-arrow-down"></i></button>
-                                </td>
-                                <td>{{ nutrient.name }}</td>
-                                <td>{{ unit(nutrient.unit) }}</td>
-                                <td>
-                                    <select v-model="nutrient.userHideSummary">
-                                        <option value="null">{{ $t("default") }} ({{ nutrient.defaultHideSummary ? $t("hide") : $t("show")}})</option>
-                                        <option value="false">{{ $t("show") }}</option>
-                                        <option value="true">{{ $t("hide") }}</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <select v-model="nutrient.userHideDetails">
-                                        <option value="null">{{ $t("default") }} ({{ nutrient.defaultHideDetails ? $t("hide") : $t("show")}})</option>
-                                        <option value="false">{{ $t("show") }}</option>
-                                        <option value="true">{{ $t("hide") }}</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                </div>
+                                <div class="col-xs-6 col-sm-3">{{ nutrient.name }}</div>
+                                <div class="col-xs-2 col-sm-1">{{ unit(nutrient.unit) }}</div>
+                                <div class="col-xs-12 col-sm-3">
+                                    <div class="row">
+                                        <div class="col-xs-4 hidden-sm hidden-md hidden-lg">{{ $t("summary") }}</div>
+                                        <div class="col-xs-8">
+                                            <select v-model="nutrient.userHideSummary">
+                                                <option value="null">{{ $t("default") }} ({{ nutrient.defaultHideSummary ? $t("hide") : $t("show")}})</option>
+                                                <option value="false">{{ $t("show") }}</option>
+                                                <option value="true">{{ $t("hide") }}</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-3">
+                                    <div class="row">
+                                        <div class="col-xs-4 hidden-sm hidden-md hidden-lg">{{ $t("details") }}</div>
+                                        <div class="col-xs-8">
+                                            <select v-model="nutrient.userHideDetails">
+                                                <option value="null">{{ $t("default") }} ({{ nutrient.defaultHideDetails ? $t("hide") : $t("show")}})</option>
+                                                <option value="false">{{ $t("show") }}</option>
+                                                <option value="true">{{ $t("hide") }}</option>
+                                            </select>
+                                        </div>
+                                </div>
+                            </div>
+                            <div class="row"><div class="col-xs-12"><br /></div></div>
+                        </template>
+                    </template>
                 </div>
             </div>
+            
             <hr />
             <div class="row">
                 <div class="col-sm-12">
@@ -130,4 +150,5 @@ module.exports = {
 <style scoped>
     .nutrient-settings thead th { text-align: center;}
     .nutrient-settings td { padding: 1px 5px;}
+    .center { text-align: center; }
 </style>
