fe1935b0467457368d8b719cb165ccb581b7a647
mikekoder <mkoderfi@gmail.com>
2017-09-26 20:33:27 +0300

    Exercise name to workout model

--- Crash.Fit.Core/Training/ITrainingRepository.cs
+++ Crash.Fit.Core/Training/ITrainingRepository.cs
@@ -17,7 +17,7 @@ namespace Crash.Fit.Training
         bool DeleteExercise(Exercise exercise);
         bool RestoreExercise(Guid id, out ExerciseDetails exercise);
 
-        IEnumerable<WorkoutSummary> SearchWorkouts(Guid userId, DateTimeOffset start, DateTimeOffset end);
+        IEnumerable<WorkoutDetails> SearchWorkouts(Guid userId, DateTimeOffset start, DateTimeOffset end);
         WorkoutDetails GetWorkout(Guid id);
         bool CreateWorkout(WorkoutDetails workout);
         bool UpdateWorkout(WorkoutDetails workout);
