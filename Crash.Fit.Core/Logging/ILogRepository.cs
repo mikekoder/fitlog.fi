f5a5fdf2e8d9f1258b2c5a29f18152e528ef8ccb
Mika Kolari <mika.kolari@soikea.com>
2018-01-17 17:16:58 +0200

    Log duration

--- Crash.Fit.Core/Logging/ILogRepository.cs
+++ Crash.Fit.Core/Logging/ILogRepository.cs
@@ -8,6 +8,7 @@ namespace Crash.Fit.Logging
     public interface ILogRepository
     {
         void LogException(Guid userId, string requestMethod, string requestPath, string requestBody, Exception ex);
+        void LogDuration(string message, TimeSpan duration);
         //void LogAction(Guid userId, string action, string message, Exception ex);
         //void LogAction<T>(Guid userId, string action, string entityId, string message, Exception ex) where T : Entity;
     }
