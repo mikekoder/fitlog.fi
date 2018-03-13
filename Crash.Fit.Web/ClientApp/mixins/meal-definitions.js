6ff0e177527b4472604359f02d63f08e95cc6717
mikekoder <mkoderfi@gmail.com>
2018-03-13 18:38:57 +0200

    Mixins retry

--- Crash.Fit.Web/ClientApp/mixins/meal-definitions.js
+++ Crash.Fit.Web/ClientApp/mixins/meal-definitions.js
@@ -8,13 +8,26 @@ export default {
     },
     created() {
         var self = this;
-        this.$store.dispatch(constants.FETCH_MEAL_DEFINITIONS, {
-            success(mealDefinitions) {
-                if (self.$mealDefinitionsLoaded) {
-                    self.$mealDefinitionsLoaded(mealDefinitions);
-                }
-            },
-            failure() { }
-        });
+        var delay = 100;
+        var loader = () => {
+            if(self.isLoggedIn){
+                self.$store.dispatch(constants.FETCH_MEAL_DEFINITIONS, {
+                    success(mealDefinitions) {
+                        if (self.$mealDefinitionsLoaded) {
+                            self.$mealDefinitionsLoaded(mealDefinitions);
+                        }
+                    },
+                    failure() { }
+                });
+            }
+            else {
+                setTimeout(() => {
+                    delay = delay * 2;
+                    loader();
+                }, delay);
+            }
+        };
+
+        loader();
     }
 }
