c4502161f6b243e89b91af65f68fbe9b1282e150
unknown <admin@mikakolari.fi>
2018-05-03 19:08:28 +0300

    Search external foods by ean

---
+++ Crash.Fit.Core/External/Foodin/FoodinClient.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Crash.Fit.External.Foodin
+{
+    public class FoodinClient
+    {
+        private readonly string baseUrl;
+        public FoodinClient(string baseUrl)
+        {
+            this.baseUrl = baseUrl.TrimEnd('/');
+        }
+        public Entry GetEntry(string ean)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Accept", "application/json");
+                var json = client.DownloadString($"{baseUrl}/entry/entry_data/{ean}?product_data_language=fi");
+                return JsonConvert.DeserializeObject<Entry>(json);
+            }
+        }
+    }
+}
