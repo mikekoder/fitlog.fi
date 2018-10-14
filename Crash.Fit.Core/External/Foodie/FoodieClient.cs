c2421bff4cd9fd0e25178de83bb28dfa0e889537
Mika Kolari <mika.j.kolari@gmail.com>
2018-10-14 18:46:05 +0300

    Search foods from KRuoka

---
+++ Crash.Fit.Core/External/Foodie/FoodieClient.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crash.Fit.External.Foodie
+{
+    public class FoodieClient : IStoreClient
+    {
+        private readonly string baseUrl;
+        public FoodieClient() : this("https://www.foodie.fi")
+        {
+
+        }
+        public FoodieClient(string baseUrl)
+        {
+            this.baseUrl = baseUrl.TrimEnd('/');
+        }
+        public async Task<ExternalFood> FindFood(string ean)
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Accept", "application/json");
+                var json = client.DownloadString($"{baseUrl}/entry/entry_data/{ean}?product_data_language=fi");
+                var food = JsonConvert.DeserializeObject<Entry>(json);
+                if(food == null)
+                {
+                    return null;
+                }
+                return new ExternalFood
+                {
+                    Brand = food.BrandName,
+                    Carbohydrate = food.Carbohydrate,
+                    Ean = food.Ean,
+                    Fat = food.Fat,
+                    FatSaturated=  food.FatSaturated,
+                    Fiber = food.Fiber,
+                    Kcal = food.Kcal,
+                    Kj = food.Kj,
+                    Manufacturer = food.Subname,
+                    Name = food.Name,
+                    Protein = food.Protein,
+                    Sugar = food.Sugar
+                };
+            }
+        }
+    }
+}
---
+++ Crash.Fit.Core/External/KRuoka/ProductDetailsResponse.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crash.Fit.External.KRuoka
+{
+    public class ProductDetailsResponse
+    {
+        [JsonProperty("ean")]
+        public string Ean { get; set; }
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+        [JsonProperty("name.finnish")]
+        public string Name { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.energyKJ.amount")]
+        public decimal? Kj { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.energyKC.amount")]
+        public decimal? Kcal { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.protein.amount")]
+        public decimal? Protein { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.carbohydrates.amount")]
+        public decimal? Carbohydrate { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.carbohydratesSugar.amount")]
+        public decimal? Sugar { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.fat.amount")]
+        public decimal? Fat { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.fatSaturated.amount")]
+        public decimal? FatSaturated { get; set; }
+        [JsonProperty("nutritionalDetails.nutritionalContent.nutritionalFiber.amount")]
+        public decimal? Fiber { get; set; }
+
+        public class Nutrients
+        {
+            public decimal EnergyKj { get; set; }
+            public decimal EnergyKcal { get; set; }
+            public decimal Fat { get; set; }
+            public decimal FatSaturated { get; set; }
+        }
+    }
+}
