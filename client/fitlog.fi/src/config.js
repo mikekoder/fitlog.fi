863a9c56562ca0dd6b7245add086ec9d97e2e609
Mika Kolari <mika.j.kolari@gmail.com>
2021-06-01 11:40:56 +0300

    remove domain from api base url

--- client/fitlog.fi/src/config.js
+++ client/fitlog.fi/src/config.js
@@ -1,5 +1,4 @@
 ﻿export default {
-  apiBaseUrl: process.env.PROD ? 'https://fitlog.fi/api/' : 'https://localhost:44338/api/',
+  apiBaseUrl: process.env.PROD ? '/api/' : 'https://localhost:44338/api/',
   googleWebClientId: '947434104376-39t4p666elc7s9tlc5ac7hnec6g3oips.apps.googleusercontent.com'
-    
 }
